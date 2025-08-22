0f1eb7f72cb2d0c01b56f68749a2bfc2d290aede
REDA FATTAH <redafattah01@gmail.com>
2025-08-22 14:20:45 +0100

    ff

--- app/overview/[slug]/page.tsx
+++ app/overview/[slug]/page.tsx
@@ -1,8 +1,11 @@
 // app/vue-ensemble/[slug]/page.tsx
-
 import ClientSection from "./section-client";
 
-
-export default function Page({ params }: { params: { slug: string } }) {
-  return <ClientSection slug={params.slug} />;
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+  return <ClientSection slug={slug} />;
 }
